9cbeb634bb6187c65a00217ac47aca37ca3f7978
jihami <w2027@e-mirim.hs.kr>
2022-02-03 22:07:16 +0900

    09.Slice.py

--- 09.Slice.py
+++ 09.Slice.py
@@ -8,15 +8,16 @@ import cv2
 # cv2.waitKey()
 # cv2.destroyAllWindows()
 
-# src = cv2.imread("Image/Mybin.jpg", cv2.IMREAD_COLOR)
-#
-# dst = src.copy()
-# roi = src[100:600, 200:700]
-# dst[0:500, 0:500] = roi
-#
-# cv2.imshow("src", src)
-# cv2.imshow("dst", dst)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
+src = cv2.imread("Image/pawns.jpg", cv2.IMREAD_COLOR)
 
-#오류 -> 수정 필요
+dst = src.copy()
+roi = src[100:600, 200:700]
+dst[0:500, 0:500] = roi
+# dst[높이(행), 너비(열)] = roi를 이용해 dst 이미지 해당 영역 붙임
+
+cv2.imshow("src", src)
+cv2.imshow("dst", dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+#사진 사이즈 때문에 오류 났었음
